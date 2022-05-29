e550018e2d7d48d2c26c0722702c7f7e1a43c605
Arey <87420088+DarkArey@users.noreply.github.com>
2022-05-29 14:09:49 +0300

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -17,36 +17,36 @@ using Vector = std::vector<int>;
 
 class TNode {
 public:
-    TNode(int data, TNode *parent); //Конструктор для создания элемента на определенном узле
+    TNode(int data, TNode* parent); //Конструктор для создания элемента на определенном узле
 
     TNode();
 
     ~TNode();
 
-    void setChild(TNodePtr &child);
+    void setChild(TNodePtr& child);
 
     int getData() const;
 
-    TNode *findByValue(int value);
+    TNode* findByValue(int value);
 
-    TNode *findByValue(int value, int &shift); //Поиск элемента со смещением в shift элементов
+    TNode* findByValue(int value, int& shift); //Поиск элемента со смещением в shift элементов
 
-    Vector findPath(int value, Vector &paths);
+    Vector findPath(int value, Vector& paths);
 
     int delByValue(int value);
 
     int popLeaf();
 
-    int popLeaf(int &shift); //Выталкивание элемента со смещением в shift элементов
+    int popLeaf(int& shift); //Выталкивание элемента со смещением в shift элементов
 
-    int findDepth(TNode *root);
+    int findDepth(TNode* root);
 
     void printNode();
 
 
 private:
     int data;
-    TNode *parent;
+    TNode* parent;
     List children; //Делаем список указателей на детей узла
 };
 
@@ -58,7 +58,7 @@ public:
 
     void changeRoot(TNodePtr newRoot);
 
-    void push(int data, TNode *parent);
+    void push(int data, TNode* parent);
 
     void push(int data); //вставка корневого элемента
 
@@ -68,31 +68,31 @@ public:
 
     void print();
 
-    TNode *find(int data);
+    TNode* find(int data);
 
-    TNode *find(int data, int shift);
+    TNode* find(int data, int shift);
 
     int getCount() const;
 
     int height();
 
-    void swap(Tree &otherTree);
+    void swap(Tree& otherTree);
 
     void exclude(int value);
 
     Vector path(int value);
 
-    bool operator==(const Tree &otherTree) const;
+    bool operator==(const Tree& otherTree) const;
 
-    bool operator!=(const Tree &otherTree) const;
+    bool operator!=(const Tree& otherTree) const;
 
-    bool operator>(const Tree &otherTree) const;
+    bool operator>(const Tree& otherTree) const;
 
-    bool operator>=(const Tree &otherTree) const;
+    bool operator>=(const Tree& otherTree) const;
 
-    bool operator<(const Tree &otherTree) const;
+    bool operator<(const Tree& otherTree) const;
 
-    bool operator<=(const Tree &otherTree) const;
+    bool operator<=(const Tree& otherTree) const;
 
 private:
     TNodePtr root;
