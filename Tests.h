2f304f9ff177e8869548fabce166236bd6becc7f
Arey <87420088+DarkArey@users.noreply.github.com>
2022-05-31 19:24:19 +0300

    Update Tests.h

--- Tests.h
+++ Tests.h
@@ -1,16 +1,16 @@
 #ifndef TESTS_H
 #define TESTS_H
 
-#include <cmath>
 #include <chrono>
 using ms = std::chrono::milliseconds;
 
 #include "Tree.h"
 
 //#include 
+
 #define N 5
 #define ii 1
-#define form 0//Формат дерева 0: (ii ( ii*2 ( ii*3(...)) (ii*3(...)) (ii*3(...)) )  ( ii*2 ( ii*3(...)) (ii*3(...)) (ii*3(...)) ) ( ii*2 ( ii*3(...)) (ii*3(...)) (ii*3(...)) )) - для бинарного по два поддерева
+#define form 1//Формат дерева 0: (ii ( ii*2 ( ii*3(...)) (ii*3(...)) (ii*3(...)) )  ( ii*2 ( ii*3(...)) (ii*3(...)) (ii*3(...)) ) ( ii*2 ( ii*3(...)) (ii*3(...)) (ii*3(...)) )) - для бинарного по два поддерева
 		   //Формат дерева 1: (ii ( ii+1 ) ( ii+1 ( ii+2) ( ii+2 ( ii+3 ) ( ii+3 (...) (...) ) ) ) )
 
 void TestTree();
